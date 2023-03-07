2c98573b4ff1673907286e28ec95d5161850b4d3
sabeer <sabeermd996@gmail.com>
2023-03-07 01:21:56 +0000

    commit message

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,14 +3,25 @@ import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
+  state = {nameBtn: false}
+
+  isSubscribe = () => {
+    const {nameBtn} = this.state
+    this.setState(() => ({
+      nameBtn: !nameBtn,
+    }))
+  }
+
   render() {
+    const {nameBtn} = this.state
     return (
       <div className="container">
         <h1 className="heading">Welcome</h1>
         <p className="about-heading">Thank you! Happy learing</p>
         <button className="button" type="button" onClick={this.isSubscribe}>
-          Subscribe
+          {nameBtn}
         </button>
+        {nameBtn ? Subscribed : Subscribe}
       </div>
     )
   }
